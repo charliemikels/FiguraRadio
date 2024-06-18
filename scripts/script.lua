bfa6c2b83ac436558c9dc00ccce6414fb1bec18d
charliemikels <alk5pcjw@duck.com>
2024-06-18 15:13:43 -0400

    Radios are considered "unrendered" when beyond a set distance.

--- scripts/script.lua
+++ scripts/script.lua
@@ -1,11 +1,36 @@
 -- Auto generated script file --
 
+local max_distance_from_radios = 16
+
 local all_radios = {}
 local function pos_is_a_radio(pos)
-    local pcall_status, function_return = pcall(function (pos)
-        if client:intUUIDToString(table.unpack(world.getBlockState(pos):getEntityData()["SkullOwner"]["Id"])) == avatar:getUUID() then return true else return false end
-    end, pos)
+    local pcall_status, function_return = pcall(
+        function (pos)
+            if client:intUUIDToString(
+                table.unpack(
+                    world.getBlockState(pos):getEntityData()["SkullOwner"]["Id"]
+                )   
+            ) == avatar:getUUID() 
+            then return true else return false end
+        end,
+        pos
+    )
+
     return (pcall_status and function_return or false)
+end
+
+local function distancesquared(veca, vecb)
+    return (vecb.x - veca.x)^2 + (vecb.y - veca.y)^2 + (vecb.z - veca.z)^2
+end
+
+local max_distance_from_radios_squared = max_distance_from_radios^2
+local function radio_is_in_range(pos)
+    if      client:getViewer():isLoaded() 
+        and distancesquared(client:getViewer():getPos(), pos) < max_distance_from_radios_squared 
+    then
+        return true
+    end
+    return false
 end
 
 local world_remove_radio_loop_next_key = nil
@@ -15,7 +40,7 @@ local function world_remove_radios_loop()
     
     if current_key then 
         local current_radio = all_radios[current_key]
-        if not pos_is_a_radio(current_radio.pos) then 
+        if not (pos_is_a_radio(current_radio.pos) and radio_is_in_range(current_radio.pos)) then 
             print("lost radio at" .. tostring(current_radio.pos))
             all_radios[current_key] = nil
         end
@@ -37,7 +62,7 @@ end
 
 local function skull_renderer_loop(_, block)
     if not block then return end
-    if not pos_is_known_radio(block:getPos()) then 
+    if not pos_is_known_radio(block:getPos()) and radio_is_in_range(block:getPos()) then 
         print("Found radio at "..tostring(block:getPos()))
         print(block)
         add_radio(block:getPos())
