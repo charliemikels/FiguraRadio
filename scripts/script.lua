4421b34c4ea63bb63d0f1d9b7d67055f07cd9e58
charliemikels <alk5pcjw@duck.com>
2024-06-24 19:30:28 -0400

    radio squashes when punched

--- scripts/script.lua
+++ scripts/script.lua
@@ -1,5 +1,7 @@
 -- Auto generated script file --
 
+local radio_model = models["radio"]["Skull"]
+
 local max_distance_from_radios = 16
 
 local static_hiss = sounds["Pink-Loop"]
@@ -10,7 +12,7 @@ local all_radios = {}
 local nearest_radio = nil
 
 local brodcasts = {}
-
+local current_brodcast = nil
 
 -- Radio blocks management
 local function pos_is_a_radio(pos)
@@ -50,7 +52,8 @@ end
 
 local function add_radio(pos)
     all_radios[tostring(pos)] = {
-        pos = pos
+        pos = pos,
+        squish_scale = 1
     }
 end
 
@@ -59,25 +62,52 @@ local function reposition_sounds(pos)
     static_hiss:setPos(pos)
 end
 
+-- interaction management
+local function radio_react_to_punch(pos)
+    local current_radio = all_radios[tostring(pos)]
+    current_radio.squish_scale = 0.2
+    static_hiss:setVolume(static_hiss_punch_volume)
+end
 
--- event loops
+
+-- render loops
+local function reset_skull() 
+    radio_model:setScale(1,1,1)
+end
+
+
 local function skull_renderer_loop(_, block)
+    reset_skull()
     if not block then return end
+
+    -- test if new radio
     if not pos_is_known_radio(block:getPos()) and radio_is_in_range(block:getPos()) then 
         print("Found radio at "..tostring(block:getPos()))
         print(block)
         add_radio(block:getPos())
     end
-    -- printTable(all_radios)
+
+    local current_radio = all_radios[tostring(block:getPos())]
+    if not current_radio then 
+        -- looks like the radio isn't allways created in time (??). 
+        -- It should clear up eventualy, but lets skip for now
+        return 
+    end
+
+    -- animate punch squish effect
+    local squash = (2+(current_radio.squish_scale*-1))
+
+    radio_model:setScale(squash ,current_radio.squish_scale, squash)
+    current_radio.squish_scale = math.lerp(current_radio.squish_scale, 1, 0.2)
+    -- print(radio_model)
 end
 
--- local function tick_loop()
---     static_hiss:setPos(player:getPos())
--- end
 
+-- world tick loop
 local world_remove_radio_loop_next_key = nil
 local function world_radio_checkup_loop()
-    -- this function checks 1 radio per tick. so with many radios, it may be slow to detect changes. 
+    -- this function checks 1 radio per run. so with many radios, it may be slow to detect changes. 
+    -- but at many radios, will only use a few instructions. 
     local current_key = world_remove_radio_loop_next_key
     world_remove_radio_loop_next_key = next(all_radios, current_key)
     
@@ -94,7 +124,7 @@ local function world_radio_checkup_loop()
 
     -- test nearest radio, if viewer is arround. 
     if client:getViewer() then 
-        if      not nearest_radio or not all_radios[nearest_radio]
+        if  not nearest_radio or not all_radios[nearest_radio]
             or    distancesquared(client:getViewer():getPos(), all_radios[nearest_radio].pos) 
                 > distancesquared(client:getViewer():getPos(), current_radio.pos) 
         then
@@ -114,15 +144,25 @@ local function world_tick_loop()
     static_hiss:setVolume( math.lerp(static_hiss:getVolume(), static_hiss_volume, 0.1  ) )
 
     -- get players interacting with radios
+    local punchedRadios = {}
     for k, loopPlayer in pairs(world.getPlayers()) do
         if (loopPlayer:getSwingTime() == 1) then -- this player punched this tick
             local punchedBlock, _, _ = loopPlayer:getTargetedBlock()
             if pos_is_known_radio(punchedBlock:getPos()) then
                 print("That's a radio")
-                static_hiss:setVolume(static_hiss_punch_volume)
+                radio_react_to_punch(punchedBlock:getPos())
+                table.insert(punchedRadios, punchedBlock:getPos())
             end
         end
     end
+
+    -- someone punched a radio, try to play a brodcast. 
+    -- if #punchedRadios >= 1 then
+    --     for i, radioPos in ipairs(punchedRadios) do
+    --         print(radioPos)
+            
+    --     end
+    -- end
 end
 events.WORLD_TICK:register(world_tick_loop, "main_world_loop")
 
