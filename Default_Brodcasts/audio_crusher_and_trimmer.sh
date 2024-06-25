634c146de02d3e5d6a2eff15163d0b81b8e927bd
charliemikels <alk5pcjw@duck.com>
2024-06-25 12:53:23 -0400

    mono audio, and more aggressive compression

--- Default_Brodcasts/audio_crusher_and_trimmer.sh
+++ Default_Brodcasts/audio_crusher_and_trimmer.sh
@@ -13,8 +13,12 @@ seconds="$3"
 base_name="${filename%.*}"
 new_filename="${base_name}-${seconds}s.ogg"
 
-ffmpeg -i "$filename" -ss "$starttime" -t "$seconds" -map 0:a:0 -ar 4000 -filter:a "highpass=f=200, volume=1.5" "$new_filename"
-# crushing to 4k kills the high end. so manualy cut off the low end and boost the overall volume
+ffmpeg -i "$filename"   \
+    -ss "$starttime"    \
+    -t "$seconds"       \
+    -map 0:a:0 -ac 1               \
+    -q:a 2 -ar 4000 -filter:a "highpass=f=200, volume=1.25"    \
+    "$new_filename"
 
 # Output the result
 echo "$new_filename"
