5f742b984bed5a6171ef09d9b7d6d13c9b2df136
charliemikels <alk5pcjw@duck.com>
2024-06-25 12:31:35 -0400

    a bit less

--- Default_Brodcasts/audio_crusher_and_trimmer.sh
+++ Default_Brodcasts/audio_crusher_and_trimmer.sh
@@ -13,7 +13,7 @@ seconds="$3"
 base_name="${filename%.*}"
 new_filename="${base_name}-${seconds}s.ogg"
 
-ffmpeg -i "$filename" -ss "$starttime" -t "$seconds" -map 0:a:0 -ar 4000 -filter:a "highpass=f=200, volume=1.75" "$new_filename"
+ffmpeg -i "$filename" -ss "$starttime" -t "$seconds" -map 0:a:0 -ar 4000 -filter:a "highpass=f=200, volume=1.5" "$new_filename"
 # crushing to 4k kills the high end. so manualy cut off the low end and boost the overall volume
 
 # Output the result
